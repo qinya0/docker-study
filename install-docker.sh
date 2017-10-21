9dd82f12711a701682fc3b1d6f4b5cc531851f48
root <root@qy-1.0.2.15>
2017-10-21 15:34:23 +0800

    update sh of install-docker by user(not root)

--- install-docker.sh
+++ install-docker.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+set -ex
 
-# OS: ubuntu:14.04
+# OS: ubuntu:16.04
 
-# update the OS
-apt-get update
-apt-get install -y apt-transport-https ca-certificates
+# update 
+#sudo apt-get update
+#sudo apt-get install -y apt-transport-https ca-certificates
 
-# ready
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list
-apt-get update
+# ready for install
+#sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-# show all version for the docker-engine
-apt-cache policy docker-engine
+sudo rm -rf /etc/apt/sources.list.d/dokcer.list
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo chmod 777 /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 
-# instal docker(you can install the other version for your OS)
-apt-get install -y docker-engine=1.13.0-0~ubuntu-trusty
+sudo apt-get update
+
+# delete the old 
+sudo apt-get purge lxc-docker
+
+# search all version of docker-engine
+sudo apt-cache policy docker-engine
+
+# instal docker(you can install the other version for your system)
+sudo apt-get install -y docker-engine=1.13.0-0~ubuntu-trusty
 
 # delete useless package
-rm -rf /var/lib/apt/lists/*
+sudo rm -rf /var/lib/apt/lists/*
 
 # check docker
-docker version
+sudo docker version
