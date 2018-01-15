f5b66be459faa06050210de303720acc0baa261d
qy <qinya_0@sina.com>
2018-01-15 03:51:13 +0000

    Update get_images_registry.sh

--- shell/get_images_registry.sh
+++ shell/get_images_registry.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-image_names=$(curl -s  http://localhost:5000/v2/_catalog | awk -F'[[]' '{print $2}' |  awk -F'[]]' '{print $1}' | sed 's/\"//g' | sed 's/,/ /g')
+ip=${1:-"localhost"}
+port=${2:-"5000"}
+
+base_url="http://$ip:$port/v2"
+
+image_names=$(curl -s  ${base_url}/_catalog | awk -F'[[]' '{print $2}' |  awk -F'[]]' '{print $1}' | sed 's/\"//g' | sed 's/,/ /g')
 
 for image_name in ${image_names[@]}
 do
-   image_tags=$(curl -s  http://localhost:5000/v2/${image_name}/tags/list  | awk -F'[[]' '{print $2}' |  awk -F'[]]' '{print $1}' | sed 's/\"//g' | sed 's/,/ /g')
-   echo "$image_name"
+   image_tags=$(curl -s  ${base_url}/${image_name}/tags/list  | awk -F'[[]' '{print $2}' |  awk -F'[]]' '{print $1}' | sed 's/\"//g' | sed 's/,/ /g')
+   echo "$image_name: ${#image_tags[@]}"
    for image_tag in ${image_tags[@]}
    do
       echo "  - $image_tag"
